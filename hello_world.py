1658add26e657e68b4187f77560adbe69134a06f
Evan Lenahan <elenahan@r4services.int>
2026-03-20 09:44:31 -0500

    .\hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# hello_world.py
+# Pseudo code:
+#   1. Get user's name and age
+#   2. Print a message including the user's name and age
+#   3. Print "hello world"
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("{} is {} years old".format(name, age))
+print("hello world")
